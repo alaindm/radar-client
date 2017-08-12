1aa678ddcab6da45c0149bc380bc2e308961a5e8
Alain De Marchi <alaindemarchi@gmail.com>
2017-08-12 13:29:08 -0300

    style: change format style. Now using prettier with default settings

--- src/components/SidePane/Location.js
+++ src/components/SidePane/Location.js
@@ -1,68 +1,69 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
-import {Card, CardTitle} from 'material-ui/Card';
-import TabsWithContent from './shared/TabsWithContent'
-import PlaceIcon from 'material-ui/svg-icons/maps/place';
-import {getSingleLocationPics} from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { Card, CardTitle } from "material-ui/Card";
+import TabsWithContent from "./shared/TabsWithContent";
+import PlaceIcon from "material-ui/svg-icons/maps/place";
+import { getSingleLocationPics } from "../../actions";
 
 class Location extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
-    this.props.locationData.idOriginal !== id && this.props.getSingleLocationPics({id})
+    this.props.locationData.idOriginal !== id &&
+      this.props.getSingleLocationPics({ id });
   }
-  
+
   componentWillReceiveProps(nextProps) {
-    if(this.props.match.params !== nextProps.match.params) {
+    if (this.props.match.params !== nextProps.match.params) {
       const { id } = nextProps.match.params;
-      this.props.getSingleLocationPics({id})
-    }    
+      this.props.getSingleLocationPics({ id });
+    }
   }
 
   render() {
     const locationHeader = () => {
       return (
         <Card>
-          <CardTitle title={
-            <div 
-              onClick={ () => this.props.history.push(`/location/${this.props.locationData.id}`) } 
-              style={{
-                'cursor': 'pointer',
-                'position': 'relative'
-              }}
-              >
-              <PlaceIcon 
-                color="grey"
-                hoverColor="black"
-              />
-              <span 
+          <CardTitle
+            title={
+              <div
+                onClick={() =>
+                  this.props.history.push(
+                    `/location/${this.props.locationData.id}`
+                  )}
                 style={{
-                  position: 'absolute', 
-                  top: '50%',
-                  transform: 'translateY(-50%)',
-                  whiteSpace: 'nowrap'
+                  cursor: "pointer",
+                  position: "relative"
                 }}
+              >
+                <PlaceIcon color="grey" hoverColor="black" />
+                <span
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    transform: "translateY(-50%)",
+                    whiteSpace: "nowrap"
+                  }}
                 >
-                 {this.props.locationData.name} 
-              </span>
-            </div>
-          } />
+                  {this.props.locationData.name}
+                </span>
+              </div>
+            }
+          />
         </Card>
-      )
-    }
+      );
+    };
 
-    return (
-      <TabsWithContent header={locationHeader} />
-    )
+    return <TabsWithContent header={locationHeader} />;
   }
 }
 
 function mapStateToProps(state) {
-  return { 
+  return {
     locationData: state.insta.locationData
-   };
+  };
 }
 
-export default withRouter(connect(mapStateToProps, {getSingleLocationPics})(Location))
-
-
+export default withRouter(
+  connect(mapStateToProps, { getSingleLocationPics })(Location)
+);
--- src/components/SidePane/Post.js
+++ src/components/SidePane/Post.js
@@ -1,115 +1,124 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
-import moment from 'moment'
-import Loading from './shared/Loading'
-import Error from './Error'
-import Comments from './shared/Comments'
-import BackButton from './shared/BackButton'
-import PlaceIcon from 'material-ui/svg-icons/maps/place';
-import {Card, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import * as actions from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import moment from "moment";
+import Loading from "./shared/Loading";
+import Error from "./Error";
+import Comments from "./shared/Comments";
+import BackButton from "./shared/BackButton";
+import PlaceIcon from "material-ui/svg-icons/maps/place";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
+import * as actions from "../../actions";
 
 class Post extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
-    this.props.getPostInfo(id)
+    this.props.getPostInfo(id);
   }
-  
+
   componentWillReceiveProps(nextProps) {
-    if(this.props.match.params !== nextProps.match.params) {
+    if (this.props.match.params !== nextProps.match.params) {
       const { id } = nextProps.match.params;
-      this.props.getPostInfo(id)
-    }    
+      this.props.getPostInfo(id);
+    }
   }
 
   render() {
-    const post = this.props.postData
+    const post = this.props.postData;
     const conditionalPostLocationHeader = () => {
-      if(post.location) {
+      if (post.location) {
         return (
-          <div 
-            onClick={ () => this.props.history.push(`/location/${post.location.id}`) } 
+          <div
+            onClick={() =>
+              this.props.history.push(`/location/${post.location.id}`)}
             style={{
-              'cursor': 'pointer',
-              'position': 'relative'
+              cursor: "pointer",
+              position: "relative"
             }}
-            >
-            <PlaceIcon 
-              color="grey"
-              hoverColor="black"
-            />
-            <span 
+          >
+            <PlaceIcon color="grey" hoverColor="black" />
+            <span
               style={{
-                position: 'absolute', 
-                top: '50%',
-                transform: 'translateY(-50%)',
-                whiteSpace: 'nowrap'
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                whiteSpace: "nowrap"
               }}
-              >
+            >
               {post.location.name}
             </span>
           </div>
-        )
+        );
       }
+    };
+
+    if (this.props.loading) {
+      return <Loading />;
     }
 
-    if(this.props.loading) {
-      return (
-        <Loading />
-      )
+    if (this.props.error) {
+      return <Error />;
     }
 
-    if(this.props.error) {
-      return (
-        <Error />
-      )
-    }
-
-    if(post) {
+    if (post) {
       return (
         <div className="scroll">
           <BackButton />
           <Card className="card">
             <CardHeader
-              title={<div onClick={ () => this.props.history.push(`/user/${post.owner.username}`) } style={{'cursor': 'pointer'}}>{post.owner.full_name}</div>}
+              title={
+                <div
+                  onClick={() =>
+                    this.props.history.push(`/user/${post.owner.username}`)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {post.owner.full_name}
+                </div>
+              }
               subtitle={conditionalPostLocationHeader()}
-              avatar={post.owner.profile_pic_url}             
-            />          
+              avatar={post.owner.profile_pic_url}
+            />
             <CardMedia
-              overlay={<CardTitle title={moment(post.taken_at_timestamp * 1000).fromNow()} subtitle={`${post.edge_media_preview_like.count} likes`} />}
+              overlay={
+                <CardTitle
+                  title={moment(post.taken_at_timestamp * 1000).fromNow()}
+                  subtitle={`${post.edge_media_preview_like.count} likes`}
+                />
+              }
             >
               <img src={post.display_url} alt="from post" />
             </CardMedia>
-            <CardTitle 
-              subtitle={
-                post.edge_media_to_caption.edges.map(x => {
-                  return (
-                    <div key={x.node.text}>
-                      {x.node.text}
-                    </div>
-                  )
-                })
-              } 
+            <CardTitle
+              subtitle={post.edge_media_to_caption.edges.map(x => {
+                return (
+                  <div key={x.node.text}>
+                    {x.node.text}
+                  </div>
+                );
+              })}
             />
             <CardText>
               <Comments comments={post.edge_media_to_comment} />
             </CardText>
           </Card>
         </div>
-      )
+      );
     } else {
-      return <Loading />
+      return <Loading />;
     }
   }
 }
 
 function mapStateToProps(state) {
-  return { 
+  return {
     postData: state.insta.postData
-   };
+  };
 }
 
-export default withRouter(connect(mapStateToProps, actions)(Post))
-
-
+export default withRouter(connect(mapStateToProps, actions)(Post));
