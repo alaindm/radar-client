45a824a3ec9340f8eada2905a81eda46e4d29e98
Alain De Marchi <alaindemarchi@gmail.com>
2017-08-12 14:04:02 -0300

    feat: Improve performance when going back from a post to a region photo list by avoing an API call.

--- src/components/SidePane/Region.js
+++ src/components/SidePane/Region.js
@@ -1,40 +1,39 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import {Card, CardTitle} from 'material-ui/Card';
-import TabsWithContent from './shared/TabsWithContent'
-import * as actions from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Card, CardTitle } from "material-ui/Card";
+import TabsWithContent from "./shared/TabsWithContent";
+import * as actions from "../../actions";
 
 class Region extends Component {
   componentDidMount() {
-    this.props.getMultiLocationPics()
+    if (this.props.pics.length < 1) {
+      this.props.getMultiLocationPics();
+    }
   }
-  
+
   componentWillReceiveProps(nextProps) {
-    if(this.props.mapState !== nextProps.mapState) {
-      this.props.getMultiLocationPics()
-    } 
+    if (this.props.mapState !== nextProps.mapState) {
+      this.props.getMultiLocationPics();
+    }
   }
-  
+
   render() {
     const regionHeader = () => {
       return (
         <Card>
           <CardTitle title="Latest photos in the selected map region" />
         </Card>
-      )
-    }
-    return (
-      <TabsWithContent header={regionHeader} />
-    )
+      );
+    };
+    return <TabsWithContent header={regionHeader} />;
   }
 }
 
 function mapStateToProps(state) {
-  return { 
-    mapState: state.insta.mapState
-   };
+  return {
+    mapState: state.insta.mapState,
+    pics: state.insta.pics
+  };
 }
 
-export default connect(mapStateToProps, actions)(Region)
-
-
+export default connect(mapStateToProps, actions)(Region);
