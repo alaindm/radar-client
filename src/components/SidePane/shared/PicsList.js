c032fcd1f6d38a354bcac6e63669195b880767f0
Alain De Marchi <alaindemarchi@gmail.com>
2017-08-14 13:26:19 -0300

    feat: Add the 'rendering images message' to PicList (timeline) component based on image onLoad event

--- src/components/SidePane/shared/PicsList.js
+++ src/components/SidePane/shared/PicsList.js
@@ -1,51 +1,104 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
-import moment from 'moment'
-import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import PlaceIcon from 'material-ui/svg-icons/maps/place';
-import * as actions from '../../../actions';
-import TextWithLinks from './TextWithLinks'
-import './PicsList.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import moment from "moment";
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
+import PlaceIcon from "material-ui/svg-icons/maps/place";
+import * as actions from "../../../actions";
+import TextWithLinks from "./TextWithLinks";
+import "./PicsList.css";
 
 class PicsList extends Component {
-  renderPicCard = (item) => {
+  constructor(props) {
+    super(props);
+    this.state = { imagesLoading: true };
+  }
+
+  loadedImages = 0;
+
+  imagesLoaded = () => {
+    let totalImages = this.props.pics.length;
+    this.loadedImages++;
+    if (this.loadedImages === totalImages) {
+      this.setState({
+        imagesLoading: false
+      });
+    }
+  };
+
+  renderSpinner() {
+    if (!this.state.imagesLoading) {
+      return null;
+    }
+    return <div className="spinner">Rendering all the images ...</div>;
+  }
+
+  renderPicCard = item => {
     return (
+      <div>
+        {this.renderSpinner()}
         <Card className="card" key={item.id}>
-          {item.location && (<div onClick={ () => this.props.history.push(`/location/${item.location.id}`) } style={ {'cursor': 'pointer'} }><PlaceIcon /><span>{item.location.name}</span></div>)}
+          {item.location &&
+            <div
+              onClick={() =>
+                this.props.history.push(`/location/${item.location.id}`)}
+              style={{ cursor: "pointer" }}
+            >
+              <PlaceIcon />
+              <span>
+                {item.location.name}
+              </span>
+            </div>}
           <CardMedia
-            overlay={<CardTitle title={moment(item.date*1000).fromNow()} subtitle={`${item.likes.count} likes`} />}
+            overlay={
+              <CardTitle
+                title={moment(item.date * 1000).fromNow()}
+                subtitle={`${item.likes.count} likes`}
+              />
+            }
           >
-            <img src={item.thumbnail_src} alt="from post" />
+            <img
+              src={item.thumbnail_src}
+              onLoad={() => this.imagesLoaded()}
+              onError={() => this.imagesLoaded()}
+              alt="from post"
+            />
           </CardMedia>
-          <CardTitle  subtitle={`${item.comments.count} comments`} />
+          <CardTitle subtitle={`${item.comments.count} comments`} />
           <CardText>
             <TextWithLinks text={item.caption} />
           </CardText>
           <CardActions>
-            <FlatButton label="See Full Post" onClick={ () => this.props.history.push(`/post/${item.code}`) } />
+            <FlatButton
+              label="See Full Post"
+              onClick={() => this.props.history.push(`/post/${item.code}`)}
+            />
           </CardActions>
         </Card>
-    )
-  }
+      </div>
+    );
+  };
 
   render() {
     return (
       <div>
-        {(this.props.pics.length > 0) && this.props.pics.map(this.renderPicCard)}
+        {this.props.pics.length > 0 && this.props.pics.map(this.renderPicCard)}
       </div>
-
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
-  return { 
+  return {
     pics: state.insta.pics
-   }
-}
+  };
+};
 
-export default withRouter(connect(mapStateToProps, actions)(PicsList))
-
-
+export default withRouter(connect(mapStateToProps, actions)(PicsList));
